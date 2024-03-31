c2f9d41a0e2797cc7f31f2780fab685bc7ecaa47
qianran122 <flaming101qianran@outlook.com>
2024-03-31 23:07:36 +0800

    模组模版

---
+++ src/client/java/top/qianran/GoblinModClient.java
@@ -0,0 +1,10 @@
+package top.qianran;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class GoblinModClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/java/top/qianran/GoblinModDataGenerator.java
@@ -0,0 +1,11 @@
+package top.qianran;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class GoblinModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
